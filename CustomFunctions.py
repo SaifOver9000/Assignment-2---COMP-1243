e101d29d291a87f5bc888b8a798d69efcda4b723
number1rhymefan <0312089b@acadiau.ca>
2026-02-10 12:51:37 -0400

    Update CustomFunctions.py
    Added MedianCost, MinCost, and MaxCost functions

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -1,1 +1,32 @@
 #contains our custom functions
+
+import statistics
+
+def MedianCost(cost):
+  """
+  Calculates the median cost of a list of costs
+
+  Arguments:
+  cost (list)
+  """
+  medianCost = statistics.median(costs)
+  return(f"The median cost per bag is ${medianCost}")
+
+def MinCost(cost):
+  """
+  Calculates the min cost from a list of costs
+
+  Arguments:
+  cost (list)
+  """
+  minCost = min(cost)
+  return(f"The minimum cost per bag is ${minCost}")
+def MaxCost(cost):
+  """
+  Calculates the max cost from a list of costs
+
+  Arguments:
+  cost (list)
+  """
+  maxCost = max(cost)
+  return(f"The maximum cost per bag is ${maxCost}")
