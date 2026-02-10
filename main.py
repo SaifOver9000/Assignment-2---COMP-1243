bd2f725b63ab5040145df7f8ec19c2c6e08d7344
number1rhymefan <0312089b@acadiau.ca>
2026-02-10 13:18:18 -0400

    Update main.py
    added base loop, will refine later

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 """
-Here we will run the functions created in functions.py
+Here we will run the functions created in CustonFunctions.py
 We wil also test the thing here
 """
+costs = []
+weights = []
+names = []
+done = ""
+
+#main stuff
+while done != "y" or done != "Y":
+    names.append(input("What is the name of the item in letters?: "))
+    costs.append(float(input("What is the cost of the item in dollars?: ")))
+    weights.append(float(input("What is the weight of the item in grams?: ")))
+    done = input("Are you done? (Y/n): ")
